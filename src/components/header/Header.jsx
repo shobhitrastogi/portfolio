8bfafcf190a009d5c832998f85cca07314e505f8
shobhitrastogi <rshobhit255@gmail.com>
2023-12-13 14:09:07 +0530

    Changes for deploy

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Projects from './../projects/Projects';
-import Experience from './../experience/Experience';
 
 const Header = () => {
   return (
